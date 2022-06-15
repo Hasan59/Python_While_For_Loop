829b45c617124a912bef34082a98dd393fec1790
Hasan Mahmud <1059hasan@gmail.com>
2022-06-15 17:26:51 +0600

    The skytouching

--- main.py
+++ main.py
@@ -26,6 +26,25 @@ while i < 6:
       else:
         print("i is no longer less than 6")
 
+        fruits = ["apple", "banana", "cherry"]
+        for x in fruits:
+          print(x)
+
+          for x in "banana":
+            print(x)
+
+            fruits = ["apple", "banana", "cherry"]
+            for x in fruits:
+              print(x)
+              if x == "banana":
+                break
+
+                fruits = ["apple", "banana", "cherry"]
+                for x in fruits:
+                  if x == "banana":
+                    break
+                  print(x)
+
 
 
 
